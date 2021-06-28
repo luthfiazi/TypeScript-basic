6c7e654af3667d64bf77defbe271b82b60023e8b
luthfiazi <luthfiazizi11@gmail.com>
2021-06-28 17:26:25 +0700

    tipe data array

---
+++ src/typeDataArray.ts
@@ -0,0 +1,11 @@
+//hanya menggunakan tipe data number pada aray
+let arr: number[];
+arr = [1, 2, 3]
+
+//hanya menggunakan tipe data string pada array
+let arr2: string[];
+arr2 = ['motor', 'mobil', 'sepeda']
+
+//bisa dipakai menggunakan tipe data apa saja
+let arr3: any[];
+arr3 = ['1', 'motor', true, {}]
